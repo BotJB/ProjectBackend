736159f589ad5223a94e1551f540d6ec899f8636
BotJB <69150052+BotJB@users.noreply.github.com>
2024-07-09 10:23:17 -0400

    Backend fixed bugs

--- Controllers/usersControllers.js
+++ Controllers/usersControllers.js
@@ -4,33 +4,31 @@ const csv = require("fast-csv");
 const fs = require("fs");
 const BASE_URL = process.env.BASE_URL
 
-// register user
 exports.userpost = async (req, res) => {
-    const file = req.file.filename;
+    const file = req.file ? req.file.filename : null;
     const { fname, lname, email, mobile, gender, location, status } = req.body;
 
     if (!fname || !lname || !email || !mobile || !gender || !location || !status || !file) {
-        res.status(401).json("All Inputs is required")
+        return res.status(400).json("All inputs are required.");
     }
 
     try {
         const preuser = await users.findOne({ email: email });
 
         if (preuser) {
-            res.status(401).json("This user already exist in our databse")
-        } else {
-
-            const datecreated = moment(new Date()).format("YYYY-MM-DD hh:mm:ss");
-
-            const userData = new users({
-                fname, lname, email, mobile, gender, location, status, profile: file, datecreated
-            });
-            await userData.save();
-            res.status(200).json(userData);
+            return res.status(409).json("This user already exists in our database.");
         }
+
+        const datecreated = moment(new Date()).format("YYYY-MM-DD hh:mm:ss");
+
+        const userData = new users({
+            fname, lname, email, mobile, gender, location, status, profile: file, datecreated
+        });
+        await userData.save();
+        res.status(200).json(userData);
     } catch (error) {
-        res.status(401).json(error);
-        console.log("catch block error")
+        console.error("Catch block error:", error);
+        res.status(500).json("An error occurred while processing your request.");
     }
 };
 
